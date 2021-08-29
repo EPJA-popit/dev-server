99de983a0325c2004298f9b7e75afd329dd15c1f
KamilRizatdinov <k.rizatdinov@innopolis.university>
2021-08-29 13:49:48 +0300

    feat: add get request handler

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const { getModuleData } = require('./utils/module-package');
 const app = express();
 
+
 const startServer = ({ port }) => {
+  const moduleData = getModuleData();
+  const appPath = `/${moduleData.cleanName}`;
+
+  app.get(appPath, function (req, res) {
+    res.send('Hello, ' + appPath.replace('/', ''));
+  })
+
   app.listen(port, () => {
-    console.log(`❤️❤️❤️  Server started listening on http://localhost:${port}/  ❤️❤️❤️`);
+    console.log(`❤️❤️❤️  Server started listening on http://localhost:${port}${appPath}  ❤️❤️❤️`);
   })
 }
 
