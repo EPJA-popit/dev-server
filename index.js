76fb724e7038d84e3b29596129252c1153544834
KamilRizatdinov <k.rizatdinov@innopolis.university>
2021-09-04 17:28:55 +0300

    feat: add applyHbs to development server

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const { getModuleData } = require('./utils/module-package');
+const applyHbs = require('@popit/templates');
 const app = express();
 
 
 const startServer = ({ port }) => {
   const moduleData = getModuleData();
   const appPath = `/${moduleData.cleanName}`;
+  applyHbs(app);
 
   app.get(appPath, function (req, res) {
+    res.render('index', {
+      staticUrl: '/static',
+      fireAppVersion: '1.0.0',
+      apps: { 
+        foo: { 
+          version: '1.0.0', 
+          name: 'foo' 
+        } 
+      },
+      navigations: {
+        'dummy.main': '/dummy',
+        'dummy.login': '/dummy/login',
+      },
+      config: {},
+      title: 'Popit app',
+    })
     res.send('Hello, ' + appPath.replace('/', ''));
   })
 
