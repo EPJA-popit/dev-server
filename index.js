662dd84d412e563334404c21392ff3c25381bdb2
KamilRizatdinov <k.rizatdinov@innopolis.university>
2021-09-06 00:05:17 +0300

    feat: lecture 3

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const webpack = require('webpack');
+const path = require('path');
+const webpackDevMiddleWare = require('webpack-dev-middleware');
 const { getModuleData } = require('./utils/module-package');
 const applyHbs = require('@popit/templates');
 const app = express();
+
+const staticUrl = '/static';
 
 
 const startServer = ({ port }) => {
@@ -9,25 +14,57 @@ const startServer = ({ port }) => {
   const appPath = `/${moduleData.cleanName}`;
   applyHbs(app);
 
+  console.log(appPath);
+
+  const compiler = webpack({
+    mode: 'development',
+    entry: './src/index.tsx',
+    output: {
+      filename: 'index.js',
+      path: path.resolve('dist'),
+      libraryTarget: 'umd',
+      publicPath: '/static/dummy/1.0.0/'
+    },
+    resolve: {
+      extensions: ['.tsx', '.js', '.ts', '.jsx', '.json ']
+    },
+    module: {
+      rules: [{
+        test: /\.tsx?$/,
+        loader: 'ts-loader'
+      }]
+    } 
+  });
+
+  app.use(webpackDevMiddleWare(compiler, {
+    publicPath: '/static/dummy/1.0.0/'
+  }));
+
   app.get(appPath, function (req, res) {
     res.render('index', {
-      staticUrl: '/static',
-      fireAppVersion: '1.0.0',
+      staticUrl: staticUrl,
+      fireAppVersion: '1.0.0/dist',
       apps: { 
-        foo: { 
-          version: '1.0.0', 
-          name: 'foo' 
+        dummy: { 
+          version: '1.0.0',
         } 
       },
       navigations: {
-        'dummy.main': '/dummy',
+        'dummy': '/dummy',
         'dummy.login': '/dummy/login',
       },
       config: {},
       title: 'Popit app',
-    })
-    res.send('Hello, ' + appPath.replace('/', ''));
-  })
+    });
+  });
+
+  app.use(
+    staticUrl,
+    express.Router().get(
+      /\/([.-\w]+)\/([.-\w\d]+)\/(.*)/, 
+      require('./utils/get-module')
+    )
+  );
 
   app.listen(port, () => {
     console.log(`❤️❤️❤️  Server started listening on http://localhost:${port}${appPath}  ❤️❤️❤️`);
---
+++ utils/get-module.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+const getLocalModulePath = (libName, filePath) => {
+  return path.resolve('../node_modules', libName, filePath)
+}
+
+module.exports = (req, res) => {
+  const libName = req.params[0];
+  const libVersion = req.params[1];
+  const libFilePath = req.params[2];
+
+  let filePath;
+
+  if (libName === 'fire.app') {
+    filePath = getLocalModulePath(`@popit/${libName}`, libFilePath);
+  } else {
+    filePath = getLocalModulePath(libName, libFilePath);
+  }
+
+  console.log(filePath);
+
+  res.sendFile(filePath);
+}
